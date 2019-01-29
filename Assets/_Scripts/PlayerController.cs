b5c74689e423b90999a4e541be974dcbddfd30a4
Milan Singh <milanprsingh@gmail.com>
2019-01-29 13:27:41 -0500

    Player movement implemented, bouncy physics implemented

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -15,9 +15,11 @@ public class PlayerController : MonoBehaviour
 
     // Horizontal Accelleration on input
     public float startAcc;
+    
+    public float jumpForce;
 
     // Threshold for horiz input to be considered
-    [Range(1e-6f, 0.1f)]
+    [Range(0.05f, 0.5f)]
     public float moveThreshold;
 
     // Bounce Resitition
@@ -26,35 +28,58 @@ public class PlayerController : MonoBehaviour
 
     // Velocity
     private Vector2 v;
+    private Rigidbody2D rb;
+
+    private bool bouncy;
+
+    public float radius;
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xIn = Input.GetAxis("Horizontal");
+        UpdateScaleFromRadius();
+
+        float xIn = Input.GetAxisRaw("Horizontal");
         MoveOnInput(xIn);
-        transform.Translate(v * Time.deltaTime);
+        // TODO: Replace to particle-wise application once CD is done
+        v.y = rb.velocity.y;
+        rb.velocity = v;
+
+        if (Input.GetKeyUp(KeyCode.Space)) Jump();
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            GetComponent<CircleCollider2D>().isTrigger = true;
+            bouncy = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            GetComponent<CircleCollider2D>().isTrigger = false;
+            bouncy = false;
+        }
     }
 
     void MoveOnInput( float xIn )
     {
-        xIn = xIn > 0.25f ? Mathf.Sign(xIn) : 0;
+        xIn = Mathf.Abs(xIn) > 0.25f ? Mathf.Sign(xIn) : 0;
         if (Mathf.Abs(xIn) > moveThreshold)
         {
             if (Mathf.Sign(xIn) == Mathf.Sign(v.x) || Mathf.Abs(v.x) < moveThreshold)
             {
                 float vX = v.x;
-                vX += xIn * startAcc * Time.time;
-                Debug.Log( v.x + " += " + xIn + " * " + startAcc + " * " + Time.deltaTime);
-                Debug.Log( "vX: " + vX );
-                v.x = ClampAbs(vX, maxHorizSpeed);
+                vX += xIn * startAcc * Time.deltaTime;
+                v.x = ClampAbs(vX, maxHorizSpeed); 
             }
             else
             {
@@ -63,9 +88,22 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            //v.x += -1 * Mathf.Sign(v.x) * stopDecc * Time.deltaTime;
-           // v.x = (Mathf.Abs(v.x) < moveThreshold) ? 0 : v.x;
+            v.x = (Mathf.Abs(v.x) < moveThreshold) ? 0 : v.x;
+            if (Mathf.Abs(v.x) < moveThreshold)
+            {
+                v.x = 0;
+            }
+            else
+            {
+                v.x += -1 * Mathf.Sign(v.x) * stopDecc * Time.deltaTime;
+            }
         }
+    }
+
+    void Jump()
+    {
+        // TODO: Replace with own Force Accumulator after CD is done
+        rb.AddForce(Vector2.up * jumpForce);
     }
 
     float MaxAbs( float f0, float f1 )
@@ -76,5 +114,53 @@ public class PlayerController : MonoBehaviour
     float ClampAbs( float val, float maxVal)
     {
         return Mathf.Abs(val) > Mathf.Abs(maxVal) ? Mathf.Sign(val) * Mathf.Abs(maxVal) : val;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (bouncy)
+        {
+            if (other.tag == "Level")
+            {
+                // TODO: Generalise CircleCast radius
+                RaycastHit2D contact = Physics2D.CircleCast(transform.position, transform.lossyScale.x * 0.5f, rb.velocity, 10.0f, ~LayerMask.GetMask("Player"));
+                if (contact.collider != null)
+                {
+                    float vMag = rb.velocity.magnitude;
+                    if (vMag > moveThreshold)
+                    {
+                        Vector2 nNorm = contact.normal.normalized;
+                        Vector2 vNorm = rb.velocity.normalized;
+                        Vector2 r = vNorm - 2 * Vector2.Dot(vNorm, nNorm) * nNorm;
+                        r = r * vMag;
+                        v = r;
+                        rb.velocity = r;
+                    }
+                }
+            }
+        }
+    }
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.tag == "Level")
+        {
+            RaycastHit2D contact = Physics2D.CircleCast(transform.position, transform.lossyScale.x * 0.5f, rb.velocity, 10.0f, ~LayerMask.GetMask("Player"));
+            if (contact.collider != null)
+            {
+                Vector2 toContact = new Vector2(transform.position.x, transform.position.y) - contact.point;
+                if (toContact.sqrMagnitude < radius * radius) {
+                    transform.position = contact.point + toContact.normalized * radius;
+                    Vector2 normalPerp = new Vector2(-contact.normal.y, contact.normal.x);
+                    float vAlongNormPerp = Vector2.Dot(normalPerp, v);
+                    v = normalPerp * vAlongNormPerp;
+                    rb.velocity = v;
+                }
+            }
+        }
+    }
+
+    private void UpdateScaleFromRadius()
+    {
+        float diameter = 2 * radius;
+        transform.localScale = new Vector3(diameter, diameter, diameter);
     }
 }
