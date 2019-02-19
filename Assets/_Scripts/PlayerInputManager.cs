ff491617e3ec240c95875832907f9f0ba3a28aab
milanprsingh <milanprsingh@gmail.com>
2019-02-18 21:39:19 -0500

    Began generalisation of Input management:
    Primary Ideas:
     -Two primary function types, KeyInput() and AxisInput(float)
     -Dictionaries map Key/Axis to function pointer (delegate)
     -Need a dictionary to store current bindings
     -Need some way of looking up functions by string, as well as
     distinguising Key Name from Axis Name.

--- Assets/_Scripts/PlayerInputManager.cs
+++ Assets/_Scripts/PlayerInputManager.cs
@@ -4,16 +4,67 @@ using UnityEngine;
 
 public class PlayerInputManager : MonoBehaviour
 {
-    private MovementController moveController;
+    private MovementController movementController;
+
+    // Determines wheter Input.GetAxis or Input.GetAxisRaw is used
+    public bool useRawAxisInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        moveController = GetComponent<MovementController>();
+        movementController = GetComponent<MovementController>();
     }
+
+    private delegate void AxisInputFunction(float axis);
+    private delegate void KeyInputFunction();
+
+    // Default Key bindings here, key/axis -> function
+    Dictionary<string, string> bindings = new Dictionary<string, string>() 
+    { 
+        { "Horizontal", "AccelerateOnInput" },
+        { "Space", "Jump" }
+    };
+
+    Dictionary<KeyCode, KeyInputFunction> keyUpMappings = new Dictionary<KeyCode, KeyInputFunction>();
+    Dictionary<KeyCode, KeyInputFunction> keyMappings = new Dictionary<KeyCode, KeyInputFunction>();
+    Dictionary<KeyCode, KeyInputFunction> keyDownMappings = new Dictionary<KeyCode, KeyInputFunction>();
+    Dictionary<string, AxisInputFunction> axisMappings = new Dictionary<string, AxisInputFunction>();
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Call Axis functions with appropriate input
+        foreach( string axis in axisMappings.Keys )
+        {
+            float axisValue = 0;
+            if (useRawAxisInput)
+            {
+                axisValue = Input.GetAxisRaw(axis);
+            }
+            else
+            {
+                axisValue = Input.GetAxis(axis);
+            }
+            axisMappings[axis](axisValue);
+        }
+
+        // Call Button Up methods for keys pressed
+        foreach ( KeyCode key in keyUpMappings.Keys )
+        {
+            if (Input.GetKeyUp(key)) keyUpMappings[key]();
+        }
+
+        // Call GetKey methods for keys pressed
+        foreach (KeyCode key in keyMappings.Keys)
+        {
+            if (Input.GetKey(key)) keyMappings[key]();
+        }
+
+        // Call Key Downmethods for keys pressed
+        foreach (KeyCode key in keyDownMappings.Keys)
+        {
+            if (Input.GetKeyDown(key)) keyDownMappings[key]();
+        }
+
     }
 }
