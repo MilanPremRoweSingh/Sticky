7e2d37c2b5304a3d1f86547f28f064e8a002c437
Milan Singh <milanprsingh@gmail.com>
2019-02-24 13:21:09 -0500

    Added Script with Button to gen Colliders:
    ColliderBuilderEditor needs to put on the parent of all level objects
    which are 3D and need a 2D polygon collider generated. Tag is used to
    distinguish objects which do and dont need a collider generated
    
    ColliderBuilder still needs to be  implemented

---
+++ Assets/_Scripts/EditorScripts/ColliderBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderBuilder : MonoBehaviour
+{
+    public string tagForGeneration;
+
+    public void GenerateLevelColliders()
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject childObj = child.gameObject;
+            if (childObj.tag == tagForGeneration)
+            {
+                Collider2D coll2D = GenerateCollider2DForObject(childObj);
+            }
+        }
+    }
+
+    // Creates a PolygonCollider for the object \
+    public static Collider2D GenerateCollider2DForObject(GameObject obj)
+    {
+        // Ensure the obj has a mesh filter with a non-null mesh
+        MeshFilter meshFilter = obj.GetComponent<MeshFilter>();
+        if (meshFilter == null || meshFilter.sharedMesh == null) return null;
+
+        Mesh mesh = meshFilter.sharedMesh;
+
+        return null;
+    }
+    
+}
---
+++ Assets/_Scripts/EditorScripts/ColliderBuilderEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ColliderBuilder))]
+public class ColliderBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ColliderBuilder myScript = (ColliderBuilder)target;
+        if (GUILayout.Button("Generate Colliders"))
+        {
+            myScript.GenerateLevelColliders();
+        }
+    }
+}
